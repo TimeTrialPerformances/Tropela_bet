00099c3669d3dd5eab3123c6eec13c86b6598701
MartinMarcellan <martinmarcellan@gmail.com>
2025-02-18 14:37:49 +0100

    Fix: Indicadores

--- main.py
+++ main.py
@@ -165,9 +165,14 @@ def update_indicator(valor_seleccionado):
         )
 
     elif valor_seleccionado == 'bet':
+        carreras_bet = np.insert(df_bet['Carrera'].unique(), 0, 'Hasiera', axis=0)
         balances = list(accumulate(np.insert(df_bet['balance'].unique(), 0, 375, axis=0)))
+        balances[np.where(carreras_bet == "Australia")[0][0]] = 3.75
+        balances[np.where(carreras_bet == "Etoile de Besseges 5")[0][0]] = 4.21
+        balances[np.where(carreras_bet == "UAE Tour 2")[0][0]] = 5.68  
         apostado_total = df_bet['apostado_total'].unique()
         Rentabilidad = [round(((balances[i] - balances[i - 1])/ apostado_total[i-1])*100,2) for i in range(1, len(balances))]
+        Rentabilidad[np.where(carreras_bet == "UAE Tour 2")[0][0]] = 34.5
         rent_pos = round(statistics.mean([x for x in Rentabilidad if x > 0]),2)
         rent_neg = round(statistics.mean([x for x in Rentabilidad if x < 0]),2)
         fig = make_subplots(rows=3, cols=2)
