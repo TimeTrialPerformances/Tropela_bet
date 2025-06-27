e9bb4b18510a65675ee4e99b14a38171b9436bfd
MartinMarcellan <martinmarcellan@gmail.com>
2025-06-27 20:34:58 +0200

    fixx

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ df_tropela = pd.read_csv('datos_tropela.csv', sep=';')
 df_tropela = df_tropela.replace('Beniat', 'Beñat')
 df_tropela['Porcentaje'] = round((df_tropela['Puntos'] / df_tropela['Puntos_ganador']) * 100,2)
 
-df_bet = pd.read_csv('bet.csv',sep=',')
-df_bet['cuota_tot'] = df_bet.groupby(['Carrera'])['Resultado'].transform(lambda x: (x.sum() - len(x)))
-df_bet['apostado_total'] = df_bet.groupby(['Carrera'])['Apostado'].transform(lambda x: (x * len(x)))
+# df_bet = pd.read_csv('bet.csv',sep=',')
+# df_bet['cuota_tot'] = df_bet.groupby(['Carrera'])['Resultado'].transform(lambda x: (x.sum() - len(x)))
+# df_bet['apostado_total'] = df_bet.groupby(['Carrera'])['Apostado'].transform(lambda x: (x * len(x)))
 # df_bet['balance'] = df_bet['cuota_tot'] * df_bet['Apostado']
 
 # try:
@@ -102,30 +102,39 @@ def update_indicator(valor_seleccionado):
         fig.add_trace(go.Scatter(x=carreras,y=puntos_martin,mode='lines+markers',line=dict(color=color_martin),name='Martin'))
         width = '900px'
     elif valor_seleccionado == 'bet':
-        carreras_bet = np.insert(df_bet['Carrera'].unique(), 0, 'Hasiera', axis=0)
-        # balances = list(accumulate(np.insert(df_bet['balance'].unique(), 0, 375, axis=0)))
-        balances = np.insert(df_bet.groupby('Carrera', as_index=False,sort=False).first()['total'], 0, 375, axis=0)
-        # balances[np.where(carreras_bet == "Australia")[0][0]] = 3.75
-        # balances[np.where(carreras_bet == "Etoile de Besseges 5")[0][0]] = 4.21
-        # balances[np.where(carreras_bet == "UAE 2")[0][0]] = 5.68  
-        # balances[np.where(carreras_bet == "Algarve 5")[0][0]] = 6.62
-        balances_25 = [round(x / 15,2) for x in balances]
-        balances_50 = [round(x / 7.5,2) for x in balances]
-        apostado_total = df_bet['apostado_total'].unique()
-        # Rentabilidad = [round(((balances[i] - balances[i - 1])/ apostado_total[i-1])*100,2) for i in range(1, len(balances))]
-        Rentabilidad = np.insert(df_bet.groupby('Carrera', as_index=False,sort=False).first()['Rentabilidad'],0, 0, axis= 0)
-        # Rentabilidad[np.where(carreras_bet == "UAE 2")[0][0]] = 34.5
-        # Rentabilidad[np.where(carreras_bet == "Algarve 5")[0][0]] = 16.54
+        # carreras_bet = np.insert(df_bet['Carrera'].unique(), 0, 'Hasiera', axis=0)
+        # # balances = list(accumulate(np.insert(df_bet['balance'].unique(), 0, 375, axis=0)))
+        # balances = np.insert(df_bet.groupby('Carrera', as_index=False,sort=False).first()['total'], 0, 375, axis=0)
+        # # balances[np.where(carreras_bet == "Australia")[0][0]] = 3.75
+        # # balances[np.where(carreras_bet == "Etoile de Besseges 5")[0][0]] = 4.21
+        # # balances[np.where(carreras_bet == "UAE 2")[0][0]] = 5.68  
+        # # balances[np.where(carreras_bet == "Algarve 5")[0][0]] = 6.62
+        # balances_25 = [round(x / 15,2) for x in balances]
+        # balances_50 = [round(x / 7.5,2) for x in balances]
+        # apostado_total = df_bet['apostado_total'].unique()
+        # # Rentabilidad = [round(((balances[i] - balances[i - 1])/ apostado_total[i-1])*100,2) for i in range(1, len(balances))]
+        # Rentabilidad = np.insert(df_bet.groupby('Carrera', as_index=False,sort=False).first()['Rentabilidad'],0, 0, axis= 0)
+        # # Rentabilidad[np.where(carreras_bet == "UAE 2")[0][0]] = 34.5
+        # # Rentabilidad[np.where(carreras_bet == "Algarve 5")[0][0]] = 16.54
 
-        fig = go.Figure()
+        # fig = go.Figure()
 
-        fig.add_trace(go.Scatter(x = carreras_bet, y = balances_25,mode='lines+markers',line=dict(color='seagreen'),name='1.Taldea'))
+        # fig.add_trace(go.Scatter(x = carreras_bet, y = balances_25,mode='lines+markers',line=dict(color='seagreen'),name='1.Taldea'))
 
-        fig.add_trace(go.Scatter(x = carreras_bet, y = balances_50,mode='lines+markers',line=dict(color='darkviolet'),name='2.Taldea'))
+        # fig.add_trace(go.Scatter(x = carreras_bet, y = balances_50,mode='lines+markers',line=dict(color='darkviolet'),name='2.Taldea'))
 
-        fig.add_trace(go.Bar(x = carreras_bet, y = Rentabilidad, name = 'Errent.', text = Rentabilidad, 
-                            marker_color='indianred',textposition='outside'))
-        width = '1300px'
+        # fig.add_trace(go.Bar(x = carreras_bet, y = Rentabilidad, name = 'Errent.', text = Rentabilidad, 
+        #                     marker_color='indianred',textposition='outside'))
+        # width = '1300px'
+        fig = go.Figure(go.Box(x = [0,1,2,3,4,5], marker_color = 'lightseagreen', name= '',boxpoints='all'))
+
+        width = '0px'
+        height = '0px'
+        display = 'none'
+
+        fig.update_layout(
+        margin=dict(l=0,r=0,b=0,t=0),
+        )
 
 
     fig.update_layout(
@@ -291,25 +300,34 @@ def update_indicator(valor_seleccionado,carrera_seleccionada):
 
 
     elif valor_seleccionado == 'bet':
-        df_giro7 = df_bet.loc[df_bet['Carrera'] == carrera_seleccionada]
-        colors = [['lime' if val != 0 else 'tomato' for val in df_giro7['Resultado']] for _ in range(9)]
-        fig = go.Figure(data=[go.Table(
-        columnwidth = [70,100,35,35,100,100,40,30,60],
-        header=dict(values=['Lehiaketa', 'A Txirrindularia', 'A Kuota', 'B Kuota', 'B Txirrindularia', 'Igarpena', 'Portzentaia', 'Emaitza', 'Jokatutako dirua'],
-                    fill_color='khaki',
-                    align='left'),
-        cells=dict(values=[df_giro7.Carrera, df_giro7.Corredor_1, df_giro7.Cuota_1, df_giro7.Cuota_2, df_giro7.Corredor_2, df_giro7.Prediccion, df_giro7.Porcentaje, df_giro7.Resultado, df_giro7.Apostado],
-                fill_color=colors,
-                align='left'))
-        ])
+        # df_giro7 = df_bet.loc[df_bet['Carrera'] == carrera_seleccionada]
+        # colors = [['lime' if val != 0 else 'tomato' for val in df_giro7['Resultado']] for _ in range(9)]
+        # fig = go.Figure(data=[go.Table(
+        # columnwidth = [70,100,35,35,100,100,40,30,60],
+        # header=dict(values=['Lehiaketa', 'A Txirrindularia', 'A Kuota', 'B Kuota', 'B Txirrindularia', 'Igarpena', 'Portzentaia', 'Emaitza', 'Jokatutako dirua'],
+        #             fill_color='khaki',
+        #             align='left'),
+        # cells=dict(values=[df_giro7.Carrera, df_giro7.Corredor_1, df_giro7.Cuota_1, df_giro7.Cuota_2, df_giro7.Corredor_2, df_giro7.Prediccion, df_giro7.Porcentaje, df_giro7.Resultado, df_giro7.Apostado],
+        #         fill_color=colors,
+        #         align='left'))
+        # ])
+
+        # fig.update_layout(
+        # margin=dict(l=0,r=0,b=0,t=0),
+        # barmode='stack'
+        # )
+        # width = '1200px'
+        # height = '250px'
+        # display = 'block'
+        fig = go.Figure(go.Box(x = [0,1,2,3,4,5], marker_color = 'lightseagreen', name= '',boxpoints='all'))
+
+        width = '0px'
+        height = '0px'
+        display = 'none'
 
         fig.update_layout(
         margin=dict(l=0,r=0,b=0,t=0),
-        barmode='stack'
         )
-        width = '1200px'
-        height = '250px'
-        display = 'block'
 
 
     return fig, {'width': width, 'height': height, 'display': display}
